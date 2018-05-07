7f2005c3495e1b2e747ffcecd3ab5aa4c24488dc
wanghongcheng <2466022993@qq.com>
2018-05-07 19:25:40 +0800

    fixbug

--- src/main/java/com/hongcheng/fruitmall/common/mail/MailBody.java
+++ src/main/java/com/hongcheng/fruitmall/common/mail/MailBody.java
@@ -15,7 +15,7 @@ public class MailBody {
         text += "<a href='" + SiteConstant.SITE_URL + "'><img src='" + SiteConstant.SITE_ICO + "' width='179' height='80' /></a>";
         text += "<p class='sayhi'>您好，" + userName + "，欢迎成为 fruitMall 用户。</p>";
         text += "<p>请点击下面的链接对您的邮箱进行验证：</p>";
-        text += "<p><a href='" + link + "' target='_blank'>" + link + "</a></p>";
+        text += "<p><a href='" + link + ">" + link + "</a></p>";
         text += "<p class='fcol_999'>(此链接24小时内有效，超时需要重新获取验证邮件)</p>";
         text += "<p>如果该链接无法点击，请将其复制粘贴到您的浏览器地址栏中访问。</p>";
         text += "<p>如此邮件非您本人操作请忽略。</p>";
@@ -36,7 +36,7 @@ public class MailBody {
         String subject = "密码找回";
         text += "<p class='sayhi'>您好 " + userName + "，</p>";
         text += "<p>请点击下面的链接找回密码:</p>";
-        text += "<p><a href='" + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + "' target='_blank'>" + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + "</a></p>";
+        text += "<p><a href='" + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + ">" + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + "</a></p>";
         text += "<p class='fcol_999'>(此链接24小时内有效，超时需要重新获取验证邮件)</p>";
         text += "<p>如果该链接无法点击，请将其复制粘贴到您的浏览器地址栏中访问。</p>";
         text += "<p>如此邮件非您本人操作请忽略。</p>";
@@ -57,7 +57,7 @@ public class MailBody {
         text += "<p class='sayhi'>您好 " + userName + "，</p>";
         text += "<p>你在本店预约的 "+productTitle+" 我们开始卖了，快来买呀！</p>";
         text += "<a href='" + SiteConstant.SITE_URL + "'><img src='" + imgUrl + "' width='179' height='80' /></a>";
-        text += "<p><a href='" + SiteConstant.SITE_URL+" target='_blank'>" + SiteConstant.SITE_URL+ "</a></p>";
+        text += "<p><a href='" + SiteConstant.SITE_URL+" >" + SiteConstant.SITE_URL+ "</a></p>";
         text += "<p>如果该链接无法点击，请将其复制粘贴到您的浏览器地址栏中访问。</p>";
         text += "<p>如此邮件非您本人操作请忽略。</p>";
         text += "<p>祝使用愉快！</p>";
