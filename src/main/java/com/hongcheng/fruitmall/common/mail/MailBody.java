f444e50d0509351c6b861dead1f658191dc6894b
wanghongcheng <2466022993@qq.com>
2018-05-08 18:01:59 +0800

    邮件bug

--- src/main/java/com/hongcheng/fruitmall/common/mail/MailBody.java
+++ src/main/java/com/hongcheng/fruitmall/common/mail/MailBody.java
@@ -15,7 +15,7 @@ public class MailBody {
         text += "<a href='" + SiteConstant.SITE_URL + "'><img src='" + SiteConstant.SITE_ICO + "' width='179' height='80' /></a>";
         text += "<p class='sayhi'>您好，" + userName + "，欢迎成为 fruitMall 用户。</p>";
         text += "<p>请点击下面的链接对您的邮箱进行验证：</p>";
-        text += "<p><a href='" + link + ">" + link + "</a></p>";
+        text += "<p><a href=" + link + '>' + link + "</a></p>";
         text += "<p class='fcol_999'>(此链接24小时内有效，超时需要重新获取验证邮件)</p>";
         text += "<p>如果该链接无法点击，请将其复制粘贴到您的浏览器地址栏中访问。</p>";
         text += "<p>如此邮件非您本人操作请忽略。</p>";
@@ -34,9 +34,9 @@ public class MailBody {
         String text = "<style>p{color:#555;font-size:16px; line-height:20px;}.sayhi{font-weight:bold;}p.fcol_999{color:#999;}p a{font:16px arial; color:#003399;}</style>";
         text += "<a href='" + SiteConstant.SITE_URL + "'><img src='" + SiteConstant.SITE_ICO + "' width='179' height='80' /></a>";
         String subject = "密码找回";
-        text += "<p class='sayhi'>您好 " + userName + "，</p>";
+        text += "<p class='sayhi'>您好 " + "尊敬的顾客" + "，</p>";
         text += "<p>请点击下面的链接找回密码:</p>";
-        text += "<p><a href='" + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + ">" + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + "</a></p>";
+        text += "<p><a href=" + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + '>' + SiteConstant.SITE_URL + "/pwdset.html?email=" + email + "&code=" + code + "</a></p>";
         text += "<p class='fcol_999'>(此链接24小时内有效，超时需要重新获取验证邮件)</p>";
         text += "<p>如果该链接无法点击，请将其复制粘贴到您的浏览器地址栏中访问。</p>";
         text += "<p>如此邮件非您本人操作请忽略。</p>";
